e7a8f794e91bee75dcf102104da11cadffdfdacb
rutwik10 <rutwikgosavi@gmail.com>
2023-04-07 17:15:39 +0530

    Summary component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Meals from "./components/Meals/Meals";
 
 import Header from "./components/Layout/Header";
 
@@ -6,6 +7,9 @@ function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
