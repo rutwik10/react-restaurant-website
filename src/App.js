1bcaa63e741fa44548e69e8b22b8d4e2375ab634
rutwik10 <rutwikgosavi@gmail.com>
2023-04-08 17:10:01 +0530

    cart items page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
+
 import Header from "./components/Layout/Header";
+import CartProvider from "./store/CartProvider";
 
 function App() {
 
@@ -17,13 +19,13 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
